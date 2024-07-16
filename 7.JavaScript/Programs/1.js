8fc612def9edbc944fb2dce32cf0a2686689d787
mohdriyaan <mohammedrayaan1@gmail.com>
2024-07-16 19:18:24 +0000

    files commit

---
+++ 7.JavaScript/Programs/1.js
@@ -0,0 +1,9 @@
+// JS Program to print Hello World
+
+// 1st Method
+console.log("Hello World")
+
+// 2nd Method
+let a = "Hello World"
+console.log(a)
+
---
+++ 7.JavaScript/Programs/2.js
@@ -0,0 +1,10 @@
+// JS Program To Add 2 Numbers
+import readline from "readline-sync"
+
+let a = readline.questionFloat("Enter the first number: ")
+let b = readline.questionFloat("Enter the second number: ")
+
+console.log(`The Addition Of ${a} and ${b} is ${a+b}`)
+
+
+
